e02b88c2703efaa8d57e1dac78d63ae8983056c5
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-11-29 00:54:58 -0800

    Finished my ship factory function that passes all of it's tests

--- src/battleship-logic/Ship.js
+++ src/battleship-logic/Ship.js
@@ -1,3 +1,21 @@
-export default function Ship() {
-  return {};
+export default function Ship(length) {
+  if (length <= 0) return null;
+  const hits = [];
+  function hit(position) {
+    if (position >= length || position < 0 || hits.includes(position)) return;
+    hits.push(position);
+  }
+  function isHit(position) {
+    return hits.includes(position);
+  }
+  function isSunk() {
+    return hits.length === length;
+  }
+  return {
+    length,
+    hits,
+    isSunk,
+    isHit,
+    hit,
+  };
 }
