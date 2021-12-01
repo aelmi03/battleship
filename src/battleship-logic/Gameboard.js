8a1568bd60ff59472ede3da1e1c29e12e44c7690
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2021-12-01 01:10:01 -0800

    Finished my placeShip function and got it passing all of it's tests with the help of many helper functions

--- src/battleship-logic/Gameboard.js
+++ src/battleship-logic/Gameboard.js
@@ -1,3 +1,62 @@
+import Ship from './Ship';
+
 export default function GameBoard() {
-  return {};
+  const ships = [];
+  const coordinates = [[], [], [], [], [], [], [], [], [], [], []];
+  const missedCoordinates = [];
+  function coordinatesAreInRange(shipCoordinates) {
+    for (let i = 0; i < shipCoordinates.length; i += 1) {
+      // eslint-disable-next-line prefer-destructuring
+      const specificCoordinates = shipCoordinates[i];
+      const [firstCoordinate, secondCoordinate] = specificCoordinates;
+      if (
+        // eslint-disable-next-line operator-linebreak
+        firstCoordinate > 9 ||
+        // eslint-disable-next-line operator-linebreak
+        firstCoordinate < 0 ||
+        // eslint-disable-next-line operator-linebreak
+        secondCoordinate > 9 ||
+        secondCoordinate < 0
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function noDuplicateCoordinates(shipCoordinates) {
+    const newSet = new Set(
+      // eslint-disable-next-line comma-dangle
+      shipCoordinates.map((coordinate) => JSON.stringify(coordinate))
+    );
+    return newSet.size === shipCoordinates.length;
+  }
+
+  function coordinatesAreAllowed(shipCoordinates) {
+    if (
+      // eslint-disable-next-line operator-linebreak
+      !coordinatesAreInRange(shipCoordinates) ||
+      !noDuplicateCoordinates(shipCoordinates)
+    ) {
+      return false;
+    }
+    for (let i = 0; i < shipCoordinates.length; i += 1) {
+      // eslint-disable-next-line prefer-destructuring
+      const specificCoordinates = shipCoordinates[i];
+      const [firstCoordinate, secondCoordinate] = specificCoordinates;
+      if (coordinates[firstCoordinate][secondCoordinate]) return false;
+    }
+    return true;
+  }
+  function placeShip(...shipCoordinates) {
+    if (!coordinatesAreAllowed(shipCoordinates)) return;
+    const newShip = Ship(shipCoordinates.length);
+    ships.push(newShip);
+    for (let i = 0; i < shipCoordinates.length; i += 1) {
+      // eslint-disable-next-line prefer-destructuring
+      const specificCoordinates = shipCoordinates[i];
+      const [firstCoordinate, secondCoordinate] = specificCoordinates;
+      coordinates[firstCoordinate][secondCoordinate] = [newShip, i];
+    }
+  }
+  return { placeShip, missedCoordinates, coordinates };
 }
--- src/battleship-logic/tests/Gameboard.test.js
+++ src/battleship-logic/tests/Gameboard.test.js
@@ -23,7 +23,7 @@ describe('Gameboard', () => {
     expect(gameBoard.coordinates[0][1]).toBeUndefined();
     gameBoard.placeShip([0, 0], [1, 0]);
     expect(gameBoard.coordinates[0][0]).not.toBeUndefined();
-    expect(gameBoard.coordinates[0][1]).not.toBeUndefined();
+    expect(gameBoard.coordinates[1][0]).not.toBeUndefined();
   });
   test("Won't place the ship if there is already a ship in the coordinates", () => {
     gameBoard.placeShip([0, 0], [1, 0], [2, 0]);
@@ -31,12 +31,16 @@ describe('Gameboard', () => {
     expect(gameBoard.coordinates[1][0]).not.toBeUndefined();
     expect(gameBoard.coordinates[2][0]).not.toBeUndefined();
     gameBoard.placeShip([0, 0], [0, 1], [0, 2]);
-    expect(gameBoard.coordinates[(0, 1)]).toBeUndefined();
-    expect(gameBoard.coordinates[(0, 2)]).toBeUndefined();
+    expect(gameBoard.coordinates[0][1]).toBeUndefined();
+    expect(gameBoard.coordinates[0][2]).toBeUndefined();
   });
   test('Will not place the ship if given copies of the same coordinates', () => {
     gameBoard.placeShip([0, 0], [0, 0]);
-    expect(gameBoard.coordinates[(0, 0)]).toBeUndefined();
+    expect(gameBoard.coordinates[0][0]).toBeUndefined();
+    gameBoard.placeShip([2, 0], [2, 0]);
+    expect(gameBoard.coordinates[2][0]).toBeUndefined();
+    gameBoard.placeShip([0, 0], [0, 1]);
+    expect(gameBoard.coordinates[0][0]).not.toBeUndefined();
   });
   test('Will record the coordinates of the missed shots', () => {
     gameBoard.placeShip([1, 0], [2, 0], [3, 0]);
@@ -45,13 +49,13 @@ describe('Gameboard', () => {
     gameBoard.receiveAttack(4, 0);
     expect(gameBoard.missedCoordinates[0]).toEqual([4, 0]);
   });
-  test('The Gameboard is able to pass the coordinates of a hit to the appropriate ship', () => {
+  test.skip('The Gameboard is able to pass the coordinates of a hit to the appropriate ship', () => {
     gameBoard.placeShip([9, 0], [9, 1], [9, 2]);
     gameBoard.receiveAttack(9, 0);
     expect(gameBoard.coordinates[9][0][0].isHit(0)).toBe(true);
     expect(gameBoard.coordinates[9][1][0].isHit(1)).toBe(false);
   });
-  test('Gameboard is able to pass the correct coordinates of a hit multiple times', () => {
+  test.skip('Gameboard is able to pass the correct coordinates of a hit multiple times', () => {
     gameBoard.placeShip([5, 0], [6, 0], [7, 0], [8, 0]);
     gameBoard.receiveAttack(5, 0);
     gameBoard.receiveAttack(6, 0);
@@ -59,7 +63,7 @@ describe('Gameboard', () => {
     gameBoard.receiveAttack(8, 0);
     expect(gameBoard.coordinates[5][0][0].isSunk()).toBe(true);
   });
-  test("Gameboard is able to know when all of it's ships are sunk with just one ship", () => {
+  test.skip("Gameboard is able to know when all of it's ships are sunk with just one ship", () => {
     gameBoard.placeShip([3, 0], [3, 1], [3, 2]);
     gameBoard.receiveAttack(3, 0);
     gameBoard.receiveAttack(3, 1);
@@ -68,7 +72,7 @@ describe('Gameboard', () => {
     expect(gameBoard.allShipsAreSunk()).toBe(true);
   });
 
-  test("Gameboard is able to know when all of it's ships are sunk with multiple ships", () => {
+  test.skip("Gameboard is able to know when all of it's ships are sunk with multiple ships", () => {
     gameBoard.placeShip([0, 0], [0, 1], [0, 2]);
     gameBoard.placeShip([1, 0], [2, 0], [3, 0]);
     gameBoard.receiveAttack(0, 0);
